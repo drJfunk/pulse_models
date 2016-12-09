6142117a6e9d6e04694dc4a943f733bc8465f06a
J. Michael Burgess <jmichaelburgess@gmail.com>
2016-12-09 16:34:16 +0100

    added setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+    
+from distutils.core import setup
+from distutils.extension import Extension
+
+
+from distutils.core import setup
+
+
+setup(name='pulse_models',
+      version='0.1',
+      description='Pulse shape models for astromodels',
+      author='J. Michael Burgess',
+      author_email='jmichaelburgess@gmail.com',
+      packages=['pulse_models'],
+     )
